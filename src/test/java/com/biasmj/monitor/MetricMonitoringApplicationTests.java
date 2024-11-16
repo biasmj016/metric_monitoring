639f7ded09f97afe7231df054801c0a9ed1ab468
biasmj016 <mjkim031@gmail.com>
2024-11-17 00:48:26 +0900

    feat: 기본 프로젝트 세팅

---
+++ src/test/java/com/biasmj/monitor/MetricMonitoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biasmj.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetricMonitoringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
